a813b2b6772e6db7df4d30850225578521888588
System Administrator <admin@example.com>
2019-05-29 21:09:41 +0000

    Changes by 'System Administrator' on 'cicdtest2 – 1.0.0' -- Build #1

---
+++ sn/cicdtest2/Script Include/active_true/x_snc_cicdtest2.testcicd.js
@@ -0,0 +1,29 @@
+/* 
+ * Application : cicdtest2
+ * ClassName   : sys_script_include
+ * Created On  : 2019-05-24 23:24:24
+ * Created By  : admin
+ * Updated On  : 2019-05-29 17:19:03
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=173295badbe57300e87dd49fdf961935
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_snc_cicdtest2.module:sys_script_include
+ */
+//new code test here
+var testcicd = Class.create();
+testcicd.prototype = /** @lends x_snc_cicdtest2.module:sys_script_include.testcicd.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+    type: 'testcicd'
+};
